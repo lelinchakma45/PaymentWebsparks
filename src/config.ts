b1759ad3b4cd7cffe4d14069f8d1b131ea4ec8c5
Ridoy01768884132 <ridoy302583@gmail.com>
2024-11-17 16:47:07 +0600

    Dist Folder

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,4 @@
-export const API_BASE_URL = 'http://127.0.0.1:8000'
-export const PRICING_URL = 'https://www.facebook.com'
-export const CHAT_URL = 'https://www.google.com'
+export const API_BASE_URL = 'http://api.websparks.ai'
+export const PRICING_URL = 'https://subscription.websparks.ai'
+export const CHAT_URL = 'https://chat.websparks.ai'
 export const MAIN_URL = 'https://www.websparks.ai'
