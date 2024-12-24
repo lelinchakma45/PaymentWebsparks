e2375e366aa646fa472ee19aecd5f94658dbbfcf
Ridoy302583 <103106417+Ridoy302583@users.noreply.github.com>
2024-12-24 13:20:31 +0600

    hello

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,4 @@
 export const API_BASE_URL = 'https://api.websparks.ai'
 export const PRICING_URL = 'https://subscription.websparks.ai'
 export const CHAT_URL = 'https://chat.websparks.ai'
-export const MAIN_URL = 'https://www.websparks.ai'
+export const MAIN_URL = 'https://websparks.ai'
